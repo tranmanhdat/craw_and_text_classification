c3f68a0a82e520f58cee1462e2852218f0528ad3
trandat <tranmanhdathsgs@gmail.com>
2021-03-24 10:36:47 +0700

    add files

--- classify_text/utils.py
+++ classify_text/utils.py
@@ -185,8 +185,8 @@ def text_preprocess(document):
 def process_text(f_in, f_out):
     f_read = codecs.open(f_in, "r", encoding='utf-8')
     text = f_read.read()
-    text = text_preprocess(text)
     if len(text) > 20:
+        text = text_preprocess(text)
         f_write = codecs.open(f_out, "w+", encoding='utf-8')
         f_write.write(text)
         f_write.close()
