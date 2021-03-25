ba12517ae6003b0eb51b4a8585422a9d018013f6
trandat <tranmanhdathsgs@gmail.com>
2021-03-25 09:12:16 +0700

    update

--- classify_text/4_remove_stop_words.py
+++ classify_text/4_remove_stop_words.py
@@ -4,28 +4,44 @@ import sys
 import os
 import time
 
+
 def load_stop_words(stop_words_file):
     with open(stop_words_file, "r", encoding="UTF-8") as f_read:
         lines = f_read.readlines()
         stop_words = []
-        for i in range(2,21):
+        for i in range(2, 21):
             stop_words.append(lines[i].split("\t")[0])
         return stop_words
 
+
+def load_remove_words(freq_words_file):
+    with open(freq_words_file, "r", encoding="UTF-8") as f_read:
+        lines = f_read.readlines()
+        remove_words = []
+        for line in lines:
+            pieces = line.split("\t")
+            if int(pieces[1])<21:
+                remove_words.append(pieces[0])
+        return remove_words
+
+
 stop_words = load_stop_words("freq_text/stop_words.txt")
+remove_words = load_remove_words("freq_text/freq_words.txt")
+need_remove_words = stop_words + list(set(remove_words) - set(stop_words))
 
 def remove_stopwords(f_in, f_out):
     f_read = codecs.open(f_in, "r", encoding='utf-8')
     text = f_read.read()
     words = []
     for word in text.split():
-        if word not in stop_words:
+        if word not in need_remove_words:
             words.append(word)
     text = ' '.join(words)
     f_write = codecs.open(f_out, "w+", encoding='utf-8')
     f_write.write(text)
     f_write.close()
     f_read.close()
+
 
 if __name__ == '__main__':
     folder_in = sys.argv[1]
